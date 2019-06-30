73878ff19a7c811a7403c4fcddc5355fc85fdfb8
Katie Rose <katiefelten@gmail.com>
2019-06-30 15:24:45 -0400

    practiced: variable mutations and type coercion

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,36 @@
+// var firstName = 'John';
+// console.log(firstName);
+
+// var lastName = 'Smith';
+// var age = 28;
+
+// var fullAge = true;
+// console.log(fullAge)
+
+// var job;
+// console.log(job)
+
+// job = 'Teacher';
+// console.log(job)
+
+// Variable mutation and type coercion
+
+var firstName = 'John';
+var age = 28;
+
+//Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried; 
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+// pop up
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
