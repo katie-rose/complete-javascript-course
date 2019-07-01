23141de7c16413d4b5251ae98c4d8c865b3d1a9b
Katie Rose <katiefelten@gmail.com>
2019-06-30 22:44:39 -0400

    practiced ternary and switch statements

--- 2-JS-basics/starter/script-2.js
+++ 2-JS-basics/starter/script-2.js
@@ -39,15 +39,64 @@
 
 /*** Boolean Logic ****/
 
-var firstName = 'John';
-var age = 20;
+// var firstName = 'John';
+// var age = 20;
 
- if (age < 13) {
-     console.log(firstName + ' is a boy.');
- } else if (age >= 13 && age < 20) {
-     console.log(firstName + ' is a teenager.');
- } else if (age >= 20 && age < 30) {
-     console.log(firstName + ' is a young man.');
- } else {
-     console.log(firstName + ' is a man.');
- }
+//  if (age < 13) {
+//      console.log(firstName + ' is a boy.');
+//  } else if (age >= 13 && age < 20) {
+//      console.log(firstName + ' is a teenager.');
+//  } else if (age >= 20 && age < 30) {
+//      console.log(firstName + ' is a young man.');
+//  } else {
+//      console.log(firstName + ' is a man.');
+//  }
+
+/*** The Ternary Operator and Switch Statements ****/
+
+var firstName = 'John'; 
+var age = 14;
+
+// *** Ternary operator 
+age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+// if (age >=18) {
+//     var drink = 'beer';
+// } else {
+//     var drink = 'juice';
+// }
+
+// *** Switch statement
+var job = 'teacher';
+switch (job) {
+  case "teacher":
+  case "instructor":
+    console.log(firstName + " teaches kids how to code.");
+    break;
+  case "driver":
+    console.log(firstName + " drives an Uber.");
+    break;
+  case "designer":
+    console.log(firstName + " makes websites.");
+    break;
+    default: console.log(firstName + ' does something else.');
+}
+
+age = 56;
+switch (true) {
+  case age < 13:
+    console.log(firstName + " is a boy.");
+    break;
+  case age >= 13 && age < 20:
+    console.log(firstName + " is a teen.");
+    break;
+  case age >= 20 && age < 30:
+    console.log(firstName + " is a young man.");
+    break;
+  default:
+    console.log(firstName + " is a man.");
+}
+
